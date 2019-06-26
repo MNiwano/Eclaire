7e25ef92324bae239bf4f3c50a1b879ffc23e12a
MNiwano <50729838+MNiwano@users.noreply.github.com>
2019-06-26 11:21:45 +0900

    Add files via upload

--- eclair.py
+++ eclair.py
@@ -506,7 +506,9 @@ def sigclipped_mean(data,iter=3,width=3.0,axis=None):
 
 def _sigmaclip(data,filt,width,axis):
     mean  = _filteredmean(data,filt,axis)
-    sigma = cp.sqrt(_sqm(data,mean,filt,axis=axis)/cp.sum(filt,axis=axis))
+    norm  = cp.sum(filt,axis=axis)
+    norm -= (norm>1).astype('f4')
+    sigma = cp.sqrt(_sqm(data,mean,filt,axis=axis)/norm)
 
     filt  = (width*sigma >= cp.abs(data - mean)).astype('f4')
     return filt
