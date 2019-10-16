0655f1e04d98cdcc8eb1d2da632c99d2f50b4875
MNiwano <niwano@hp.phys.titech.ac.jp>
2019-10-16 14:55:25 +0900

    Add files via upload

--- eclair/kernel.py
+++ eclair/kernel.py
@@ -3,69 +3,70 @@
 from cupy import ElementwiseKernel, ReductionKernel
 
 reduction_kernel = ElementwiseKernel(
-    'T x, T b, T d, T f',
-    'T z',
-    'z = (x - b - d) / f',
-    'reduction'
+    in_params='T x, T b, T d, T f',
+    out_params='T z',
+    operation='z = (x - b - d) / f',
+    name='reduction'
 )
 
 liner_kernel = ElementwiseKernel(
-    'T x1, T x2, T x3, T x4, T dx, T dy',
-    'T z',
-    'z= dx*dy*x1 + dx*(1-dy)*x2 + (1-dx)*dy*x3 + (1-dx)*(1-dy)*x4',
-    'linear'
+    in_params='T x1, T x2, T x3, T x4, T dx, T dy',
+    out_params='T z',
+    operation='z= dx*dy*x1 + dx*(1-dy)*x2 + (1-dx)*dy*x3 + (1-dx)*(1-dy)*x4',
+    name='linear'
 )
 
 spline_kernel = ElementwiseKernel(
-    'T u, T v, T x, T y, T d','T z',
-    'z = (u-v)*(1-d)*(1-d)*(1-d) + 3*v*(1-d)*(1-d) + (x-y-u-2*v)*(1-d) + y',
-    'spline'
+    in_params='T u, T v, T x, T y, T d',
+    out_params='T z',
+    operation='z = (u-v)*(1-d)*(1-d)*(1-d) + 3*v*(1-d)*(1-d) + (x-y-u-2*v)*(1-d) + y',
+    name='spline'
 )
 
 sum_kernel = ReductionKernel(
-    'T x, T f',
-    'T y',
-    'x*f',
-    'a+b',
-    'y=a',
-    '0',
-    'sum'
+    in_params='T x, T f',
+    out_params='T y',
+    map_expr='x*f',
+    reduce_expr='a+b',
+    post_map_expr='y=a',
+    identity='0',
+    name='sum'
 )
 
 sqm_kernel = ReductionKernel(
-    'T x, T m, T f',
-    'T y',
-    '(x-m)*(x-m)*f',
-    'a+b',
-    'y=a',
-    '0',
-    'sqared_sum'
+    in_params='T x, T m, T f',
+    out_params='T y',
+    map_expr='(x-m)*(x-m)*f',
+    reduce_expr='a+b',
+    post_map_expr='y=a',
+    identity='0',
+    name='sqared_sum'
 )
 
 median_kernel = ElementwiseKernel(
-    'T x, T f, T m',
-    'T z',
-    'z = x*f + (1-f)*m',
-    'median'
+    in_params='T x, T f, T m',
+    out_params='T z',
+    operation='z = x*f + (1-f)*m',
+    name='median'
 )
 
 clip_kernel = ElementwiseKernel(
-    'T d, T c, T s, T w',
-    'T z',
-   'if (fabsf(d-c) <= w*s) {z = 1.0;} else {z = 0.0;}',
-   'clip'
+    in_params='T d, T c, T s, T w',
+    out_params='T z',
+    operation='if (fabsf(d-c) <= w*s) {z = 1.0;} else {z = 0.0;}',
+    name='clip'
 )
 
 judge_kernel = ElementwiseKernel(
-    'T x',
-    'T z',
-    'if (x==0.0) {z=1.0;} else {z=0.0;}',
-    'judge'
+    in_params='T x',
+    out_params='T z',
+    operation='if (x==0.0) {z=1.0;} else {z=0.0;}',
+    name='judge'
 )
 
 fix_kernel = ElementwiseKernel(
-    'T m, T d, T n, T f',
-    'T z',
-    'z=m*d/(n+f)',
-    'fix'
+    in_params='T m, T d, T n, T f',
+    out_params='T z',
+    operation='z=m*d/(n+f)',
+    name='fix'
 )
